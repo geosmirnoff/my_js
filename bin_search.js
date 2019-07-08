5b15c7c1121712dd1fb9b2cde9f6480738c236a7
George Smirnov <geosmirnoff@yandex.ru>
2019-07-08 13:26:43 +0300

    Binary search

---
+++ bin_search.js
@@ -0,0 +1,36 @@
+function binSearch(list, item) {
+	
+	var low = 0;
+	var high = list.length - 1;
+	
+	var counter = 1;
+	
+	while (low <= high) {
+		
+		var mid = Math.floor((low + high) / 2);
+		var guess = list[mid];
+		
+		if (guess == item) {
+			return 'Попытка ' + counter + ': ' + list[mid] + ' - BINGO';
+		}
+		if (guess > item) {
+			high = --mid;
+			console.log('Попытка ' + counter + ': ' + guess + ' - меньше');
+		}
+		else {
+			low = ++mid;
+			console.log('Попытка ' + counter + ': ' + guess + ' - больше');
+		}
+		counter++;
+	}
+	return;
+}
+
+var my_list = [];
+for (var i = 1; i <= 100; i++)
+{
+	my_list.push(i);
+}
+
+console.log(binSearch(my_list, 34));
+
